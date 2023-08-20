b8739460287c436a362bb225818ff091c6b8d4a7
Scott Hansford <zenwattage@gmail.com>
2023-08-20 16:56:51 -0700

    fixing missing slash in RequestMapping path

--- src/main/java/com/codeday/productivity/controller/TaskController.java
+++ src/main/java/com/codeday/productivity/controller/TaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/users/{userId}/goals/{goalId}tasks")
+@RequestMapping("/v1/users/{userId}/goals/{goalId}/tasks")
 public class TaskController {
 
     @Autowired
