828a1fa04add9014432cfd60fd10e7553364df42
CourtneyHoppus <cobaltab@gmail.com>
2023-08-30 14:40:28 -0700

    updated request mapping and added logging after pulling main and testing

--- src/main/java/com/codeday/productivity/service/ReminderService.java
+++ src/main/java/com/codeday/productivity/service/ReminderService.java
@@ -4,6 +4,8 @@ import com.codeday.productivity.entity.Reminder;
 import com.codeday.productivity.entity.User;
 import com.codeday.productivity.repository.ReminderRepository;
 import com.codeday.productivity.repository.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ReminderService {
+
+    private static final Logger LOGGER = LogManager.getLogger(ReminderService.class);
 
     private final ReminderRepository reminderRepository;
     private final UserRepository userRepository;
@@ -22,12 +26,14 @@ public class ReminderService {
     }
 
     public Iterable<Reminder> getUserReminders(Integer id) {
+        LOGGER.info("Finding all Reminders for User ID: {}", id);
         User _user = userRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
         return _user.getReminders();
     }
 
     public Reminder createUserReminder(Integer id, Reminder reminder) {
+        LOGGER.info("Saving Reminder: {} to the Repo", reminder);
         User _user = userRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
         reminder.setUser(_user);
