7f28898ade5464094c65e4b7e24c65317e5c440c
CourtneyHoppus <cobaltab@gmail.com>
2023-09-15 11:13:32 -0700

    created task/sub-task relationship

--- src/main/java/com/codeday/productivity/controller/TaskController.java
+++ src/main/java/com/codeday/productivity/controller/TaskController.java
@@ -16,12 +16,11 @@ import java.util.List;
 import java.util.Objects;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("api/v1/users/{userId}/goals/{goalId}/tasks")
 public class TaskController {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(TaskController.class);
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     private final TaskService taskService;
     private final UserService userService;
@@ -35,14 +34,42 @@ public class TaskController {
     }
 
     @PostMapping
-    public Task createTask( @PathVariable Integer goalId, @RequestBody Task task) {
+    public Task createGoalTask( @PathVariable Integer goalId, @RequestBody Task task) {
         logger.info("Creating task for goal with id: " + goalId);
         Goal goal = goalService.getGoal(goalId);
         task.setGoal(goal);
         return taskService.saveTaskForUserAndGoal(goal, task);
     }
 
-
+    @PostMapping("/{id}")
+    public ResponseEntity<Task> createTaskSubTask(
+            @PathVariable Integer userId,
+            @PathVariable Integer goalId,
+            @PathVariable Integer id,
+            @RequestBody Task task) {
+        logger.info("Creating sub task for parent task with id: {}, ", id);
+        try {
+            logger.info("Getting task with id: {}, ", id);
+            Task parentTask = taskService.getTaskById(id);
+            logger.info("Getting goal with id: {}, ", goalId);
+            Goal goal = goalService.getGoal(goalId);
+            logger.info("Checking User Goal Task relationship");
+            if ((parentTask == null)
+                ||(!Objects.equals(goal.getUser().getId(), userId))
+                || (!Objects.equals(goal.getId(), goalId))) {
+                return ResponseEntity.notFound().build();
+            }
+            logger.info("Setting Task Goal to Goal ID: {}", goalId);
+            task.setGoal(goal);
+            task.setParentTask(parentTask);
+            task.setIsSubTask("Y");
+            logger.info("Save SubTask to Task ID: {}", id);
+            Task subTask = taskService.createTask(task);
+            return new ResponseEntity<>(subTask, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     // Get task by id
     @GetMapping("/{taskId}")
@@ -57,10 +84,13 @@ public class TaskController {
 
     // Get task by goal id
     @GetMapping
-    public List<Task> getAllTasksByGoal(@PathVariable int userId, @PathVariable int goalId) {
+    public ResponseEntity<List<Task>> getAllTasksByGoal(@PathVariable int userId, @PathVariable int goalId) {
         User user = userService.getUserById(userId);
         Goal goal = goalService.getGoal(goalId);
-        return taskService.getAllTasksByGoal(goal);
+        if (!Objects.equals(goal.getUser().getId(), user.getId())) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(taskService.getAllTasksByGoal(goal));
     }
 
     // Update task by id
